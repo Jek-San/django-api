632b79835a787d34023d8c0fd10b6dccd0f1f406
Jek-San <ihsanzack@gmail.com>
2022-11-07 00:50:10 +0700

    bismillah

--- customers/settings.py
+++ customers/settings.py
@@ -147,6 +147,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
