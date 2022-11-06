81c7abe1caca6b584abe6ff6e5ee6f649e340741
Jek-San <ihsanzack@gmail.com>
2022-11-06 18:28:58 +0700

    changing inside seting buat validasi untuk debug dan scretkey menggunakan os

--- customers/settings.py
+++ customers/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h@=^=(u)vl_(i*x&2j6na5bvgm2$kwlr$p4t1m8qb%@rnmjl6x'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','https://django-api-testapi.herokuapp.com/']
 
 
 # Application definition
@@ -98,13 +98,23 @@ WSGI_APPLICATION = 'customers.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
+if not DEBUG:
+    DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+else:
+    DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+
 
 
 # Password validation
