93a59312e25ae1020413e1b01333368b6f60de03
Jek-San <ihsanzack@gmail.com>
2022-11-06 19:11:31 +0700

    kembali ke semula bagian daatabase

--- customers/settings.py
+++ customers/settings.py
@@ -98,23 +98,13 @@ WSGI_APPLICATION = 'customers.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-if not DEBUG:
-    DATABASES = {
+
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-else:
-    DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
-
 
 
 # Password validation
